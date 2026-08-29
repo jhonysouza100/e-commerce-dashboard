1230fea2b40b387ddd000394c4029b36241a0dba
Jhony Souza <100396330+jhonysouza100@users.noreply.github.com>
2026-08-29 22:05:01 +0000

    feat: corregir errores tipográficos en MainContainer y ajustar estilos en ListProductsTable para mejorar la presentación

--- src/components/dashboard_layout/MainContainer.tsx
+++ src/components/dashboard_layout/MainContainer.tsx
@@ -8,12 +8,12 @@ interface MainContainerProps {
 const MainContainer: React.FC<MainContainerProps> = ({ headerContent, mainContent }) => {
 	return (
 		<div className="main_container flex justify-center items-center rounded-xl">
-      <div className="section_container w-full shadow-md rounded-md overflow-hidden min-h-[calc(100vd_-_(var(--header-height)_+_2.25rem))] lg:min-h-[calc(100vh_-_(var(--header-height)_+_5rem))]">
+      <div className="section_container w-full shadow-md rounded-md overflow-hidden min-h-[calc(100vh_-_(var(--header-height)_+_2.25rem))] lg:min-h-[calc(100vh_-_(var(--header-height)_+_5rem))]">
         <section className="header_container gap-4 h-max flex justify-end sm:justify-between items-center bg-background px-4 py-2">
           {/* Header content */}
           <>{headerContent}</>
         </section>
-        <section className="table_container grid py-3 px-4 rounded-md max-h-[calc(100vh_-_(var(--header-height)_+_5.75rem))] min-h-[calc(100vh_-_(var(--header-height)_+_5.75rem))] lg:max-h-[calc(100vh_-_(var(--header-height)_+_9.5rem))] lg:min-h-[calc(100vh_-_(var(--header-height)_+_9.5rem))] [overflow:auto_overlay]">
+        <section className="table_container grid py-3 px-4 rounded-md max-h-[calc(100vh_-_(var(--header-height)_+_8.75rem))] min-h-[calc(100vh_-_(var(--header-height)_+_8.75rem))] lg:max-h-[calc(100vh_-_(var(--header-height)_+_9.5rem))] lg:min-h-[calc(100vh_-_(var(--header-height)_+_9.5rem))] [overflow:auto_overlay]">
 					{/* Main content */}
           <>{mainContent}</>
         </section>
